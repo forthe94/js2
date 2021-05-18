424c554287fb3d632d07bc94d86f631a5cb5af29
Александр Тетенков <tetenkov@tecon.ru>
2021-05-18 19:04:41 +0300

    first cherry-pick

--- script.js
+++ script.js
@@ -1,3 +1,75 @@
+const API_URL = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses'
+
+const makeGETRequest = url => {
+    return new Promise((resolve, reject) => {
+	let xhr;
+
+        if (window.XMLHttpRequest) {
+          xhr = new XMLHttpRequest();
+        } else if (window.ActiveXObject) { 
+          xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        
+        xhr.onreadystatechange = function () {
+          if (xhr.readyState === 4) {
+            resolve(xhr.responseText);
+          }
+        }
+        
+        xhr.open('GET', url, true);
+        xhr.send();
+    })
+  
+}
+
+
+class GoodsItem {
+  constructor(product_name, price) {
+    this.product_name = product_name;
+    this.price = price;
+  }
+
+  render() {
+    return `<div class="goods-item"><h3>${this.product_name}</h3><p>${this.price}</p></div>`;
+  }
+}
+
+class GoodsList {
+  constructor() {
+    this.goods = [];
+  }
+
+  fetchGoods(cb) {
+    makeGETRequest(`${API_URL}/catalogData.json`).then ( (goods) => {
+      this.goods = JSON.parse(goods);
+      cb();
+    })
+  }
+
+  render() {
+    let listHtml = '';
+    this.goods.forEach(good => {
+      const goodItem = new GoodsItem(good.product_name, good.price);
+      listHtml += goodItem.render();
+    });
+    document.querySelector('.goods-list').innerHTML = listHtml;
+  }
+
+  getTotalSum() {
+    const totalSum = this.goods.reduce((acc, item) => {
+      if (!item.price) return acc;
+      return acc + item.price;
+    }, 0);
+    console.log(totalSum);
+  }
+}
+
+const list = new GoodsList();
+
+
+list.fetchGoods(() => {
+  list.render();
+});
 
 const goods = [
     { title: 'Shirt', price: 150 },
@@ -14,4 +86,20 @@ const goods = [
     document.querySelector('.goods-list').innerHTML = goodsList;
   }
 
-  renderGoodsList();
+  clearAll() {}
+
+  addItem() {}
+
+  removeItem() {}
+}
+
+class BasketItem extends GoodsItem {
+  constructor(...args) {
+    super(...args);
+    this.count = 0;
+  }
+
+  addOne() {}
+
+  removeOne() {}
+}
